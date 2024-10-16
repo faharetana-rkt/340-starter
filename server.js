8d3d3253f16993daf6ab19b39a15b17dc20133ec
faharetana-rkt <rakotomaharitra@gmail.com>
2024-10-16 12:44:59 +0300

    began learning 2

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 
 /* ***********************
@@ -26,6 +27,9 @@ app.use(static)
 
 // Index route
 app.get("/", baseController.buildHome)
+
+// Inventory route
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
