dc3d2c17e37fd30c769a148142bb7f2bb0706ef9
faharetana-rkt <rakotomaharitra@gmail.com>
2024-10-17 17:52:54 +0300

    finished styling of individual card for type of cars

--- utilities/index.js
+++ utilities/index.js
@@ -33,7 +33,7 @@ Util.buildClassificationGrid = async function(data){
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => {
       grid += '<li>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id
+      grid += '<a id="vehicle-anchor" href="../../inv/detail/' + vehicle.inv_id
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model
       + 'details"><img src="' + vehicle.inv_thumbnail
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model
